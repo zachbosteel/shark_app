a3a9a26618aea872d9469ebbfa5d1d68417b575e
Zach Bosteel <zbosteel@gmail.com>
2015-07-09 20:59:32 -0500

    added users for authentication purposes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
+  before_action :restrict_access
+
+  private
+
+  def restrict_access
+    authenticate_or_request_with_http_token do |api_key, options|
+      User.find_by(api_key: api_key, email: request.headers['X-User-Email'])
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630002941) do
+ActiveRecord::Schema.define(version: 20150710010547) do
 
   create_table "sharks", force: :cascade do |t|
     t.string   "common_name",        limit: 255
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20150630002941) do
     t.boolean  "suitability_as_pet", limit: 1
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email",   limit: 255
+    t.string "api_key", limit: 255
   end
 
 end
